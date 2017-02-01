16e14940353f390e2f5bd96851c4b45716a5d243
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-01-31 21:36:34 -0600

    Añadidos más colores
    16 colores disponibles

--- WinColor.py
+++ WinColor.py
@@ -3,7 +3,23 @@ import ctypes
 #~ Constantes de la API de Windows.
 STD_OUTPUT_HANDLE = -11
 
-F = 0x000F # Color Blanco Brillante.
+
+x0   = 0x0000   # Color Negro.
+x1   = 0x0001   # Color Azul.
+x2   = 0x0002   # Color Verde.
+x3   = 0x0003   # Color Aguamarina.
+x4   = 0x0004   # Color Rojo.
+x5   = 0x0005   # Color Púrpura.
+x6   = 0x0006   # Color Amarillo.
+x7   = 0x0007   # Color Blanco.
+x8   = 0x0008   # Color Gris.
+x9   = 0x0009   # Color Azul Claro.
+xA   = 0x000A   # Color Verde Claro.
+xB   = 0x000B   # Color Aguamarina Claro.
+xC   = 0x000C   # Color Rojo Claro.
+xD   = 0x000D   # Color Púrpura Claro.
+xE   = 0x000E   # Color Amarillo Claro.
+xF   = 0x000F   # Color Blanco Brillante.
 
 #~ Método que reinicia al color original
 def get_csbi_attributes(handle):
@@ -32,6 +48,6 @@ def ColorF():
 	colorF = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, reset)
 	return colorF
 
-ColorI(F)
+ColorI(xF)
 print("\n\n\t Texto De Ejemplo.")
 ColorF()
