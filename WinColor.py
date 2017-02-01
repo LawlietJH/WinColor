605310a7e1caea8ea9c3f639e552e9c290b297c3
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-02-01 00:35:22 -0600

    más detalles
    Añadidos más detalles

--- WinColor.py
+++ WinColor.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 #
+#
 #    ██╗    ██╗██╗███╗   ██╗ ██████╗ ██████╗ ██╗      ██████╗ ██████╗ 
 #    ██║    ██║██║████╗  ██║██╔════╝██╔═══██╗██║     ██╔═══██╗██╔══██╗
 #    ██║ █╗ ██║██║██╔██╗ ██║██║     ██║   ██║██║     ██║   ██║██████╔╝
@@ -9,11 +10,42 @@
 #     ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝
 #                                                                     
 #                                                         By: LawlietJH
+#                                                                 1.0.2
 
-import ctypes
+import ctypes, os
 
+BWC = r"""
+                       ╦ ╦  ┬  ┌┐┌   ╔═╗ ┌─┐ ┬   ┌─┐ ┬─┐
+                       ║║║  │  │││   ║   │ │ │   │ │ ├┬┘
+                       ╚╩╝  ┴  ┘└┘   ╚═╝ └─┘ ┴─┘ └─┘ ┴└─
+"""
+#~ Fuente: Calvin S, Página: http://patorjk.com/software/taag
+
+BA = r"""
+                            ╦  ┌─┐┬ ┬┬  ┬┌─┐┌┬┐ ╦╦ ╦
+                            ║  ├─┤││││  │├┤  │  ║╠═╣
+                            ╩═╝┴ ┴└┴┘┴─┘┴└─┘ ┴ ╚╝╩ ╩
+"""
+#~ Fuente: Calvin S, Página: http://patorjk.com/software/taag
+
+def Dat():
+	
+	Nombre = BWC
+	Autor = BA
+	Version = "\n\n{:^80}".format("1.0.3")
+	
+	return os.system("cls"),\
+	       ColorI("BB"), print("\n\n",Nombre),\
+	       ColorI("AZ"), print("\n\n",Autor),\
+	       ColorI("RC"), print(Version), ColorF(),\
+	       os.system("Pause>Nul"), exit(0)
+
+#________________________________________________________________________
+
+#________________________________________________________________________
 #~ Constantes de la API de Windows.
 STD_OUTPUT_HANDLE = -11
+
 #_______________ Colores _______________
 
 x0   = 0x0000   # Color Negro.
@@ -52,17 +84,37 @@ def get_csbi_attributes(handle):
 #~ Método Que Indica En Donde Iniciará El Color.
 def ColorI(ColorLetra):
 	
-	if ColorLetra == "xA" or ColorLetra == xA :
+	if ColorLetra == "x0" or ColorLetra == "negro" or ColorLetra == "N" :
+		Color = x0
+	elif ColorLetra == "x1" or ColorLetra == "azul" or ColorLetra == "AZ" :
+		Color = x1
+	elif ColorLetra == "x2" or ColorLetra == "verde" or ColorLetra == "V" :
+		Color = x2
+	elif ColorLetra == "x3" or ColorLetra == "aguamarina" or ColorLetra == "AG" :
+		Color = x3
+	elif ColorLetra == "x4" or ColorLetra == "rojo" or ColorLetra == "R" :
+		Color = x4
+	elif ColorLetra == "x5" or ColorLetra == "purpura" or ColorLetra == "P" :
+		Color = x5
+	elif ColorLetra == "x6" or ColorLetra == "amarillo" or ColorLetra == "AM" :
+		Color = x6
+	elif ColorLetra == "x7" or ColorLetra == "blanco" or ColorLetra == "B" :
+		Color = x7
+	elif ColorLetra == "x8" or ColorLetra == "gris" or ColorLetra == "G" :
+		Color = x8
+	elif ColorLetra == "x9" or ColorLetra == "azul cl" or ColorLetra == "AC" :
+		Color = x9
+	elif ColorLetra == "xA" or ColorLetra == "verde cl" or ColorLetra == "VC" :
 		Color = xA
-	elif ColorLetra == "xB" or ColorLetra == xB :
+	elif ColorLetra == "xB" or ColorLetra == "aguamarina cl" or ColorLetra == "AMC" :
 		Color = xB
-	elif ColorLetra == "xC" or ColorLetra == xC :
+	elif ColorLetra == "xC" or ColorLetra == "rojo cl" or ColorLetra == "RC" :
 		Color = xC
-	elif ColorLetra == "xD" or ColorLetra == xD :
+	elif ColorLetra == "xD" or ColorLetra == "purpura cl" or ColorLetra == "PC" :
 		Color = xD
-	elif ColorLetra == "xE" or ColorLetra == xE :
+	elif ColorLetra == "xE" or ColorLetra == "amarillo cl" or ColorLetra == "AC" :
 		Color = xE
-	elif ColorLetra == "xF" or ColorLetra == xF :
+	elif ColorLetra == "xF" or ColorLetra == "blanco br" or ColorLetra == "BB" :
 		Color = xF
 	else:
 		Color = reset
@@ -84,10 +136,3 @@ reset = get_csbi_attributes(handle)
 #________________________________________________________________________
 #                               By: LawlietJH
 #________________________________________________________________________
-
-
-xD = input("Color: ")
-
-ColorI(xA)
-print("\n\n\t Texto De Ejemplo.")
-ColorF()
