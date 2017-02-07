249a69e0e8881ea04b0530f611a2307d0c1d9c56
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-02-07 01:24:57 -0600

    Versión 1.1.0
    Se Actualizó la Versión a 1.1.0

--- WinColor.py
+++ WinColor.py
@@ -10,7 +10,7 @@
 #     ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝
 #                                                                     
 #                                                         By: LawlietJH
-#                                                                 1.0.7
+#                                                                 1.1.0
 
 import ctypes
 import time
@@ -37,7 +37,7 @@ def Dat():
 	
 	Nombre = BWC
 	Autor = BA
-	Version = "\n\n\n{:^80}".format("1.0.7")
+	Version = "\n\n\n{:^80}".format("1.1.0")
 	
 	return os.system("cls"), winsize(80,22),\
 	       color("VC"), print("\n\n", BWC), time.sleep(0.1),\
