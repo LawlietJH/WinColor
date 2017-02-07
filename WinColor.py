8fa74e941b4a46b3182c56cc6bf5dd722727e8b3
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-02-07 00:54:40 -0600

    Agregado " [#] " a la función Mark()
    Se le agregó la opción " [#] " a la función Mark()

--- WinColor.py
+++ WinColor.py
@@ -204,7 +204,16 @@ def Mark(Simbolo = " ", Color2 = None, Color1 = None):
 		elif Color1 == None and Color2 == None:
 			return color("VC"), print("[", end=""), color("BB"), print(Simbolo, end=""), color("VC"), print("]", end="")
 	
-
+	elif (Simbolo == "#"):
+		if Color1 != None and Color2 != None:
+			return color(Color1), print("[", end=""), color(Color2), print(Simbolo, end=""), color(Color1), print("]", end="")
+		elif Color1 != None and Color2 == None:
+			return color(Color1), print("[", end=""), color("G"), print(Simbolo, end=""), color(Color1), print("]", end="")
+		elif Color1 == None and Color2 != None:
+			return color("BB"), print("[", end=""), color(Color2), print(Simbolo, end=""), color("BB"), print("]", end="")
+		elif Color1 == None and Color2 == None:
+			return color("BB"), print("[", end=""), color("G"), print(Simbolo, end=""), color("BB"), print("]", end="")
+	
 
 
 #~ Función Que Indica En Donde Terminará El Color.
