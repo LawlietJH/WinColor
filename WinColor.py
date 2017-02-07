61772547de05ebdcfb77e2ef87cb860eb5603b97
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-02-07 00:49:34 -0600

    Agregado " [¡] " a la función Mark()
    Se le agregó la opción " [¡] " a la función Mark()

--- WinColor.py
+++ WinColor.py
@@ -174,6 +174,16 @@ def Mark(Simbolo = " ", Color2 = None, Color1 = None):
 		elif Color1 == None and Color2 == None:
 			return color("BB"), print("[", end=""), color("RC"), print(Simbolo, end=""), color("BB"), print("]", end="")
 	
+	elif (Simbolo == "¡"):
+		if Color1 != None and Color2 != None:
+			return color(Color1), print("[", end=""), color(Color2), print(Simbolo, end=""), color(Color1), print("]", end="")
+		elif Color1 != None and Color2 == None:
+			return color(Color1), print("[", end=""), color("AMC"), print(Simbolo, end=""), color(Color1), print("]", end="")
+		elif Color1 == None and Color2 != None:
+			return color("BB"), print("[", end=""), color(Color2), print(Simbolo, end=""), color("BB"), print("]", end="")
+		elif Color1 == None and Color2 == None:
+			return color("BB"), print("[", end=""), color("AMC"), print(Simbolo, end=""), color("BB"), print("]", end="")
+	
 
 
 
