ace18d695d034cb03988ee5fc57f539ba5f1a266
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-02-07 00:52:08 -0600

    Agregado " [~] " a la función Mark()
    Se le agregó la opción " [~] " a la función Mark()

--- WinColor.py
+++ WinColor.py
@@ -193,6 +193,17 @@ def Mark(Simbolo = " ", Color2 = None, Color1 = None):
 			return color("BB"), print("[", end=""), color(Color2), print(Simbolo, end=""), color("BB"), print("]", end="")
 		elif Color1 == None and Color2 == None:
 			return color("BB"), print("[", end=""), color("PC"), print(Simbolo, end=""), color("BB"), print("]", end="")
+	
+	elif (Simbolo == "~"):
+		if Color1 != None and Color2 != None:
+			return color(Color1), print("[", end=""), color(Color2), print(Simbolo, end=""), color(Color1), print("]", end="")
+		elif Color1 != None and Color2 == None:
+			return color(Color1), print("[", end=""), color("BB"), print(Simbolo, end=""), color(Color1), print("]", end="")
+		elif Color1 == None and Color2 != None:
+			return color("VC"), print("[", end=""), color(Color2), print(Simbolo, end=""), color("VC"), print("]", end="")
+		elif Color1 == None and Color2 == None:
+			return color("VC"), print("[", end=""), color("BB"), print(Simbolo, end=""), color("VC"), print("]", end="")
+	
 
 
 
