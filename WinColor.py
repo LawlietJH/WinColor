75937e1919532ac4a6dcd4db2ff508abcd308024
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-02-15 01:12:40 -0600

    Funcion Mark() actualizada
    se añadieron las funciones color() dentro de los print()

--- WinColor.py
+++ WinColor.py
@@ -265,13 +265,13 @@ def Mark(Simbolo = " ", Color2 = None, Color1 = None):
 	
 	else:
 		if Color1 != None and Color2 != None:
-			return color(Color1), print("[", end=""), color(Color2), print(Simbolo, end=""), color(Color1), print("]", end="")
+			return print("["+color(Color1), end=""), print(Simbolo+color(Color2), end=""), print("]"+color(Color1), end="")
 		elif Color1 != None and Color2 == None:
-			return color(Color1), print("[", end=""), color("BB"), print(Simbolo, end=""), color(Color1), print("]", end="")
+			return print("["+color(Color1), end=""), print(Simbolo+color("BB"), end=""), print("]"+color(Color1), end="")
 		elif Color1 == None and Color2 != None:
-			return color("BB"), print("[", end=""), color(Color2), print(Simbolo, end=""), color("BB"), print("]", end="")
+			return print("["+color("BB"), end=""), print(Simbolo+color(Color2), end=""), print("]"+color("BB"), end="")
 		elif Color1 == None and Color2 == None:
-			return color("BB"), print("[", end=""), color("BB"), print(Simbolo, end=""), color("BB"), print("]", end="")
+			return print("["+color("BB"), end=""), print(Simbolo+color("BB"), end=""), print("]"+color("BB"), end="")
 
 #________________________________________________________________________
 #                               By: LawlietJH
